ead1e3462d58d7ecfa56d5452c22af4cdfbf5036
Nazarzastavil <43075855+Nazarzastavil@users.noreply.github.com>
2019-04-19 20:47:47 +0500

    3

--- diogen/mainApp/models.py
+++ diogen/mainApp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
-'''
+
 class Person(models.Model):
-    name=models.CharField(max_lenght=200)
+    password=models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
     spec=models.IntegerField
-    phone=models.CharField(max_lenght=200)
+    phone=models.CharField(max_length=200)
     email=models.EmailField
-'''
+
+    def __str__(self):
+        return self.name
 
--- diogen/mainApp/views.py
+++ diogen/mainApp/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainApp/start_page.html')
 
+def register(request):
+    return render(request, 'mainApp/reg.html')
