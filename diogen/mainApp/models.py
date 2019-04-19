e57343dae45f524222a7062e5166c04909890523
Nazarzastavil <43075855+Nazarzastavil@users.noreply.github.com>
2019-04-19 19:58:26 +0500

    2

--- diogen/mainApp/models.py
+++ diogen/mainApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+'''
+class Person(models.Model):
+    name=models.CharField(max_lenght=200)
+    spec=models.IntegerField
+    phone=models.CharField(max_lenght=200)
+    email=models.EmailField
+'''
+
