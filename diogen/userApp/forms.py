4183293ce699973ef79157a670371a6da9c87b58
RamposhPidrov <40067659+RamposhPidrov@users.noreply.github.com>
2019-05-18 09:54:49 +0500

    dsa

--- diogen/userApp/forms.py
+++ diogen/userApp/forms.py
@@ -32,14 +32,17 @@ class ProfileForm(forms.ModelForm):
             'genres': 'Жанры Вашего творчества',
             'instruments':'Инструменты которыми вы владете',
             'soundcloud':'Ссылка на soundcloud (опционально)',
-            
+            'image': 'Ваша пикча',
             'company':'Название вашей компании',
         }
         widgets = {
             'birth_date': forms.TextInput(attrs={'class':'datepicker'}),
+            'image': forms.FileInput(attrs={'class': 'avatar'}),
             'description': forms.Textarea(attrs={'class':'materialize-textarea'})
+            
         }
         initial = {
+            
             'birth_date': ''
         }
 
